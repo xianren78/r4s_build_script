ff12658a3a7e988e4d003a144c4d6930e86259b1
闲人一枚 <qgtxqj@gmail.com>
2024-10-10 10:05:55 +0800

    linux-6.6: revert to 6.6.43 and self config

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -179,3 +179,40 @@ git clone https://$github/sbwml/package_kernel_tcp-brutal package/kernel/tcp-bru
 
 # watchcat - clean config
 true > feeds/packages/utils/watchcat/files/watchcat.config
+
+# self config below
+# remove ssrplus
+rm -rf package/new/helloworld/luci-app-ssr-plus
+rm -rf package/new/helloworld/patch-luci-app-ssr-plus.patch
+
+# add openwrt-mihomo
+rm -rf package/new/helloworld/luci-app-mihomo
+rm -rf package/new/helloworld/mihomo
+git clone https://$github/pmkol/openwrt-mihomo package/new/openwrt-mihomo
+if [ "$MINIMAL_BUILD" = "y" ]; then
+    if curl -s "https://$mirror/openwrt/23-config-minimal-common" | grep -q "^CONFIG_PACKAGE_luci-app-mihomo=y"; then
+        mkdir -p files/etc/mihomo/run/ui
+        curl -Lso files/etc/mihomo/run/GeoSite.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
+        curl -Lso files/etc/mihomo/run/GeoIP.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
+        curl -Lso files/etc/mihomo/run/geoip.metadb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb
+        curl -Lso files/etc/mihomo/run/ASN.mmdb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb
+        curl -Lso metacubexd-gh-pages.tar.gz https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
+        tar zxf metacubexd-gh-pages.tar.gz
+        rm metacubexd-gh-pages.tar.gz
+        mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
+    fi
+else
+    if curl -s "https://$mirror/openwrt/23-config-common" | grep -q "^CONFIG_PACKAGE_luci-app-mihomo=y"; then
+        mkdir -p files/etc/mihomo/run/ui
+        curl -Lso files/etc/mihomo/run/geoip.metadb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb
+        curl -Lso files/etc/mihomo/run/ASN.mmdb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb
+        curl -Lso metacubexd-gh-pages.tar.gz https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
+        tar zxf metacubexd-gh-pages.tar.gz
+        rm metacubexd-gh-pages.tar.gz
+        mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
+    fi
+fi
+
+# add ddns-go
+git clone https://$github/sirpdboy/luci-app-ddns-go package/new/ddns-go
+sed -i '/"description": "Grant UCI access for luci-app-ddns-go",/a \\t\t"order": 50,' package/new/ddns-go/luci-app-ddns-go/root/usr/share/rpcd/acl.d/luci-app-ddns-go.json
