c07f7ea1d04af7817fd65ae891a1c9e6374d8164
闲人一枚 <qgtxqj@gmail.com>
2024-11-20 08:16:30 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -42,35 +42,24 @@ sed -i '/ADDON+=USE_QUIC_OPENSSL_COMPAT=1/d' feeds/packages/net/haproxy/Makefile
 rm -rf package/new/helloworld/luci-app-mihomo
 rm -rf package/new/helloworld/mihomo
 git clone https://$github/morytyann/OpenWrt-mihomo package/new/openwrt-mihomo
-if [ "$MINIMAL_BUILD" = "y" ]; then
-    if curl -s "https://$mirror/openwrt/24-config-minimal-common" | grep -q "^CONFIG_PACKAGE_luci-app-mihomo=y"; then
-        mkdir -p files/etc/mihomo/run/ui
-        curl -Lso metacubexd-gh-pages.zip https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip
-        unzip metacubexd-gh-pages.zip
-        rm metacubexd-gh-pages.zip
-        mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
-    fi
-else
-    if curl -s "https://$mirror/openwrt/24-config-common" | grep -q "^CONFIG_PACKAGE_luci-app-mihomo=y"; then 
-        # Ensure the directories exist before extracting
-        mkdir -p files/etc/mihomo/run/ui
 
-        # Download and extract metacubexd
-        curl -Lso metacubexd-gh-pages.zip https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip
-        unzip metacubexd-gh-pages.zip
-        rm metacubexd-gh-pages.zip
-        mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
+mkdir -p files/etc/mihomo/run/ui
 
-        # Download and extract Yacd-meta-gh-pages
-        curl -Lso Yacd-meta-gh-pages.zip https://$github/MetaCubeX/yacd/archive/gh-pages.zip
-        unzip Yacd-meta-gh-pages.zip
-        mv Yacd-meta-gh-pages files/etc/mihomo/run/ui/yacd
-        rm Yacd-meta-gh-pages.zip
+# Download and extract metacubexd
+curl -Lso metacubexd-gh-pages.zip https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip
+unzip metacubexd-gh-pages.zip
+rm metacubexd-gh-pages.zip
+rm -rf files/etc/mihomo/run/ui/metacubexd
+mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
 
-        # Download and extract Razord-meta-gh-pages
-        curl -Lso Razord-meta-gh-pages.zip https://$github/MetaCubeX/Razord-meta/archive/refs/heads/gh-pages.zip
-        unzip Razord-meta-gh-pages.zip
-        mv Razord-meta-gh-pages files/etc/mihomo/run/ui/dashboard
-        rm Razord-meta-gh-pages.zip
-    fi
-fi
+# Download and extract Yacd-meta-gh-pages
+curl -Lso Yacd-meta-gh-pages.zip https://$github/MetaCubeX/yacd/archive/gh-pages.zip
+unzip Yacd-meta-gh-pages.zip
+mv Yacd-meta-gh-pages files/etc/mihomo/run/ui/yacd
+rm Yacd-meta-gh-pages.zip
+
+# Download and extract Razord-meta-gh-pages
+curl -Lso Razord-meta-gh-pages.zip https://$github/MetaCubeX/Razord-meta/archive/refs/heads/gh-pages.zip
+unzip Razord-meta-gh-pages.zip
+mv Razord-meta-gh-pages files/etc/mihomo/run/ui/dashboard
+rm Razord-meta-gh-pages.zip
