d713962bfa157431d648fb6b68657b0e28359f63
闲人一枚 <qgtxqj@gmail.com>
2025-06-13 21:23:30 +0800

    self config

--- openwrt/scripts/01-prepare_base-mainline.sh
+++ openwrt/scripts/01-prepare_base-mainline.sh
@@ -10,7 +10,8 @@ rm -rf target/linux/rockchip
 if [ "$(whoami)" = "sbwml" ]; then
     git clone https://$gitea/sbwml/target_linux_rockchip-6.x target/linux/rockchip -b openwrt-24.10
 else
-    git clone https://"$git_name":"$git_password"@$gitea/sbwml/target_linux_rockchip-6.x target/linux/rockchip -b openwrt-24.10
+    git clone https://$git_targetnew@$github/xianren78/target_linux_rockchip-6.x target/linux/rockchip -b openwrt-24.10
+#    git clone https://"$git_name":"$git_password"@$gitea/sbwml/target_linux_rockchip-6.x target/linux/rockchip -b openwrt-24.10
 fi
 
 # bpf-headers - 6.12
@@ -35,14 +36,17 @@ if [ "$(whoami)" = "sbwml" ]; then
     git clone https://$gitea/sbwml/brcmfmac-firmware-4366c-pcie package/firmware/brcmfmac-firmware-4366c-pcie
     git clone https://$gitea/sbwml/brcmfmac-firmware-4366b-pcie package/firmware/brcmfmac-firmware-4366b-pcie
 else
-    git clone https://"$git_name":"$git_password"@$gitea/sbwml/target_linux_bcm53xx target/linux/bcm53xx
-    git clone https://"$git_name":"$git_password"@$gitea/sbwml/brcmfmac-firmware-4366c-pcie package/firmware/brcmfmac-firmware-4366c-pcie
-    git clone https://"$git_name":"$git_password"@$gitea/sbwml/brcmfmac-firmware-4366b-pcie package/firmware/brcmfmac-firmware-4366b-pcie
+#    git clone https://"$git_name":"$git_password"@$gitea/sbwml/target_linux_bcm53xx target/linux/bcm53xx
+#    git clone https://"$git_name":"$git_password"@$gitea/sbwml/brcmfmac-firmware-4366c-pcie package/firmware/brcmfmac-firmware-4366c-pcie
+#    git clone https://"$git_name":"$git_password"@$gitea/sbwml/brcmfmac-firmware-4366b-pcie package/firmware/brcmfmac-firmware-4366b-pcie
+    git clone https://$git_targetnew@$github/xianren78/target_linux_bcm53xx target/linux/bcm53xx
+    git clone https://$git_targetnew@$github/xianren78/brcmfmac-firmware-4366c-pcie package/firmware/brcmfmac-firmware-4366c-pcie
+    git clone https://$git_targetnew@$github/xianren78/brcmfmac-firmware-4366b-pcie package/firmware/brcmfmac-firmware-4366b-pcie
 fi
 
 # armsr/armv8
 rm -rf target/linux/armsr
-git clone https://nanopi:nanopi@$gitea/sbwml/target_linux_armsr target/linux/armsr -b main
+git clone https://$git_targetnew@$github/xianren78/target_linux_armsr target/linux/armsr -b main
 
 # kernel - 6.12
 curl -s $mirror/tags/kernel-6.12 > include/kernel-6.12
@@ -61,7 +65,8 @@ else
     if [ "$(whoami)" = "sbwml" ]; then
         git clone https://$gitea/sbwml/target_linux_generic -b openwrt-24.10 target/linux/generic-6.12 --depth=1
     else
-        git clone https://"$git_name":"$git_password"@$gitea/sbwml/target_linux_generic -b openwrt-24.10 target/linux/generic-6.12 --depth=1
+ #       git clone https://"$git_name":"$git_password"@$gitea/sbwml/target_linux_generic -b openwrt-24.10 target/linux/generic-6.12 --depth=1
+        git clone https://$git_targetnew@$github/xianren78/target_linux_generic -b openwrt-24.10 target/linux/generic-6.12 --depth=1
     fi
 fi
 cp -a target/linux/generic-6.12/* target/linux/generic
--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,51 @@
 #!/bin/bash
 
 # 自定义脚本
+
+# remove gro improvement patch
+rm -f target/linux/generic/hack-6.12/600-net-enable-fraglist-GRO-by-default.patch
+# according to https://github.com/coolsnowwolf/lede/issues/12331
+#rm -f target/linux/generic/pending-6.12/680-net-add-TCP-fraglist-GRO-support.patch 
+
+# https://github.com/openwrt/openwrt/commit/cee749b88975fcd2861df648ff98d399e41aa1ea
+rm -f target/linux/generic/pending-6.12/620-net_sched-codel-do-not-defer-queue-length-update.patch
+# https://git.cooluc.com/sbwml/target_linux_generic/commit/51e0565f724343f69f7642a28fea97c44d28d91b
+rm -f target/linux/generic/pending-6.12/730-net-ethernet-mtk_eth_soc-reset-all-TX-queues-on-DMA-.patch
+
+# add openwrt-nikki
+rm -rf package/new/helloworld/luci-app-nikki
+rm -rf package/new/helloworld/nikki
+git clone https://$github/nikkinikki-org/OpenWrt-nikki package/new/OpenWrt-nikki
+
+# Download and extract zashboard
+mkdir -p files/etc/nikki/run/ui/zashboard
+curl -Lso zashboard.zip https://$github/Zephyruso/zashboard/releases/latest/download/dist.zip
+unzip zashboard.zip
+mv dist/* dist/.* files/etc/nikki/run/ui/zashboard/ 2>/dev/null
+rm zashboard.zip
+
+rm -rf files/etc/nikki/run/ui/metacubexd
+
+# Download and extract Yacd-meta-gh-pages
+mkdir -p files/etc/nikki/run/ui/yacd
+curl -Lso Yacd-meta-gh-pages.zip https://$github/MetaCubeX/Yacd-meta/archive/refs/heads/gh-pages.zip
+unzip Yacd-meta-gh-pages.zip
+mv Yacd-meta-gh-pages/* files/etc/nikki/run/ui/yacd/
+rm Yacd-meta-gh-pages.zip
+
+# Download and extract Razord-meta-gh-pages
+mkdir -p files/etc/nikki/run/ui/dashboard
+curl -Lso Razord-meta-gh-pages.zip https://$github/MetaCubeX/Razord-meta/archive/refs/heads/gh-pages.zip
+unzip Razord-meta-gh-pages.zip
+mv Razord-meta-gh-pages/* files/etc/nikki/run/ui/dashboard/
+rm Razord-meta-gh-pages.zip
+
+# pubkey
+mkdir -p files/root/.ssh/
+touch  files/root/.ssh/authorized_keys
+chmod 0600 files/root/.ssh/authorized_keys
+echo $PUBKEY > files/root/.ssh/authorized_keys
+
+# inputrc
+touch files/root/.inputrc
+echo "set enable-bracketed-paste off" > files/root/.inputrc
