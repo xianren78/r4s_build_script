090129a52669308e7d7efc7fd82ad4566f82f0e9
闲人一枚 <qgtxqj@gmail.com>
2026-02-22 15:59:13 +0800

    luci-app-dockerman: update to JS version
    Updated the branch for luci-app-dockerman to openwrt-25.12.

--- openwrt/scripts/00-prepare_base.sh
+++ openwrt/scripts/00-prepare_base.sh
@@ -244,7 +244,7 @@ git clone https://$github/xianren78/feeds_packages_net_curl feeds/packages/net/c
 
 # Docker
 rm -rf feeds/luci/applications/luci-app-dockerman
-git clone https://$github/sbwml/luci-app-dockerman feeds/luci/applications/luci-app-dockerman
+git clone https://$github/sbwml/luci-app-dockerman -b openwrt-25.12 feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/packages/utils/{docker,dockerd,containerd,runc}
 git clone https://$github/sbwml/packages_utils_docker feeds/packages/utils/docker
 git clone https://$github/sbwml/packages_utils_dockerd feeds/packages/utils/dockerd
